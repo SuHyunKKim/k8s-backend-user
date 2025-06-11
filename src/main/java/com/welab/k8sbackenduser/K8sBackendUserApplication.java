bd9eceff4f00c26c7629da42b2604ea37229c70b
SuHyunKKim <kimsteven728@dgu.ac.kr>
2025-06-11 15:52:31 +0900

    [feat]: alim service openfeign 연동, v2

--- src/main/java/com/welab/k8sbackenduser/K8sBackendUserApplication.java
+++ src/main/java/com/welab/k8sbackenduser/K8sBackendUserApplication.java
@@ -2,8 +2,10 @@ package com.welab.k8sbackenduser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class K8sBackendUserApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/welab/k8sbackenduser/api/open/UserController.java
+++ src/main/java/com/welab/k8sbackenduser/api/open/UserController.java
@@ -1,6 +1,7 @@
 package com.welab.k8sbackenduser.api.open;
 
 import com.welab.k8sbackenduser.common.dto.ApiResponseDto;
+import com.welab.k8sbackenduser.remote.alim.RemoteAlimService;
 import lombok.Generated;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
+    private final RemoteAlimService remoteAlimService;
+
     @GetMapping(value = "/hello")
-    public ApiResponseDto<String> test() {
-        return ApiResponseDto.createOk("웰컴 투 백엔드 유저");
+    public ApiResponseDto<String> hello() {
+
+        String remoteMessage = remoteAlimService.hello().getData();
+        String userResponse = "웰컴 투 백엔드 유저. 리모트 알림 메시지 = " + remoteMessage;
+
+        return ApiResponseDto.createOk(userResponse);
     }
 }
